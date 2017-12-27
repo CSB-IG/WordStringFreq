98f42f0bd9dca6d165cf267b0f097ee81789e8fd
Enrique Hernández-Lemus <ehernandez@inmegen.gob.mx>
2017-12-27 13:45:55 -0600

    Update Comorbodity_Counter.R

--- Comorbodity_Counter.R
+++ Comorbodity_Counter.R
@@ -1,7 +1,7 @@
 sentences<-scan("Comorbilidad.txt","character",sep="\n")
-# Reads a list of IDC Codes from a list
+# Reads a list of ICD Codes from a list
 words.freq<-table(sentences) 
-# Since IDC codes are more 'strings' than 'words', no punctuation revision is required
+# Since ICD codes are more 'strings' than 'words', no punctuation revision is required
 Z <- cbind(names(words.freq),as.integer(words.freq))
 # It compiles a "string-frequency table" useful to build Frequency vs Rank Distributions
 write.table(Z, file="conteos_comorbilidades.csv", sep=",")
